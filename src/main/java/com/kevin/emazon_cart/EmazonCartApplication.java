0f939d2533d2c19d288b1c8fbe00eb99b1fa414c
KevinMitsi <kevinandresgarciaaguirre@gmail.com>
2024-09-04 18:26:45 -0500

    first commit

---
+++ src/main/java/com/kevin/emazon_cart/EmazonCartApplication.java
@@ -0,0 +1,13 @@
+package com.kevin.emazon_cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmazonCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmazonCartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kevin/emazon_cart/EmazonCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevin.emazon_cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmazonCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
