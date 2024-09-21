ce0a1318bc4cdcb9f04c88e9a36908a1a5cd5e3a
KevinMitsi <kevinandresgarciaaguirre@gmail.com>
2024-09-20 20:19:46 -0500

    Correcciones en costantes

--- src/main/java/com/kevin/emazon_cart/domain/usecase/CartUseCase.java
+++ src/main/java/com/kevin/emazon_cart/domain/usecase/CartUseCase.java
@@ -12,19 +12,11 @@ import com.kevin.emazon_cart.infraestructure.exception.ItemIsNotInCartException;
 import com.kevin.emazon_cart.infraestructure.exception.ItemNotFoundException;
 import com.kevin.emazon_cart.infraestructure.exception.NotEnoughItemInStockResponse;
 
-import java.time.Instant;
-import java.util.Date;
 import java.util.List;
 
+import static com.kevin.emazon_cart.domain.usecase.helper.CartUseCaseHelper.*;
+
 public class CartUseCase implements ICartServicePort {
-    public static final Date ACTUAL_DATE = Date.from(Instant.now());
-    public static final String ITEM_NOT_FOUND_EXCEPTION_MESSAGE = "Este item no existe en la base de datos de Stock";
-    public static final String NOT_ENOUGH_STOCK_MESSAGE = "No hay suficientes artículos en Stock para agregar, el ultimo suministro fue el: ";
-    public static final String ADDED_TO_CART_MESSAGE = "Ha añadido a su carrito el item: ";
-    public static final String CATEGORIES_LIMIT_EXCEED_MESSAGE = "No puede agregar más de 3 artículos con la misma categoría.";
-    public static final String ITEM_NOT_IN_USER_CART_EXCEPTION_MESSAGE = "Este item no se encuentra en su carrito";
-    public static final String ZERO_STRING_VALUE = "0";
-    public static final String NO_STOCK_ITEM = "Item sin stock, supply date: ";
     private final ICartPersistentPort cartPersistentPort;
     private final IConnectionStockPort connectionStockPort;
     private final IConnectionTransactionPort connectionTransactionPort;
